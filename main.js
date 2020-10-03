49775fb5d1c8f8e2b7f9e584c55718e8cc3498d9
HaTToek <68680118+HaTToek@users.noreply.github.com>
2020-10-03 23:54:24 +0900

    두번째

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var home = document.querySelector("#home");
 var me = document.querySelector("#about_me");
 var works = document.querySelector("#works");
 var connect = document.querySelector("#connect");
+var menu = document.querySelector("#menu");
 var nav = document.querySelector("#nav");
 var prevScrollTop = 0;
 var nowScrollTop = 0;
@@ -23,35 +24,54 @@ document.addEventListener('scroll', function() {
 function btntest(btn){
     if(btn==1)
     {
+        home.style.transform = "perspective(1000px) scale(1) translate3d(0,42vh,0) rotate3d(1,0,0,72deg)";
+        home.style.opacity = "0.5";
         home.style.display = "flex";
+        nav.style.display = "none";
         me.style.display = "none";
         works.style.display = "none";
         connect.style.display = "none";
+        menu.style.display = "flex";
     }
     else if(btn==2){
         home.style.display = "none";
-        me.style.display = "block";
+        me.style.display = "flex";
         works.style.display = "none";
         connect.style.display = "none";
+        menu.style.display = "none";
+        nav.style.display = "flex";
     }
     else if(btn==3){
         home.style.display = "none";
         me.style.display = "none";
         works.style.display = "block";
         connect.style.display = "none";
+        menu.style.display = "none";
+        nav.style.display = "flex";
     }
     else if(btn==4){
         home.style.display = "none";
         me.style.display = "none";
         works.style.display = "none";
         connect.style.display = "flex";
+        menu.style.display = "none";
+        nav.style.display = "flex";
+    }else if(btn == 5){
+        home.style.transform = "perspective(0) scale(1) translate3d(0,0,0) rotate3d(0,0,0,0)";
+        home.style.opacity = "1";
+        home.style.display = "flex";
+        nav.style.display = "none";
+        me.style.display = "none";
+        works.style.display = "none";
+        connect.style.display = "none";
+        menu.style.display = "none";
     }
 }
 
 // 큰 글씨
 function randomString(){
     // let stringArr = ["Learn to HTML", "Learn to CSS", "Learn to Javascript", "Learn to Python", "Learn to Ruby"];
-    let stringArr = ["이호윤의 "]
+    let stringArr = ["이호윤의 포트폴리오 입니다."]
     let selectString = stringArr[Math.floor(Math.random() * stringArr.length)];
     let selectStringArr = selectString.split("");
     return selectStringArr;
