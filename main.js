e3a83948a0e0843ab464faf3b828f27f05a7f5bb
HaTToek <68680118+HaTToek@users.noreply.github.com>
2020-10-04 00:41:26 +0900

    3트

--- main.js
+++ main.js
@@ -12,8 +12,22 @@ var nowScrollTop = 0;
 var currentScrollValue = 0;
 var befor = 0;
 var btn = 0;
+var evn = 0;
 
+// 화면 이벤트
+function Evn(evn){
+    if (evn == 1){
+        home.style.transform = "perspective(0) scale(1) translate3d(0,0,0) rotate3d(0,0,0,0)"
+    }else if (evn == 2){
+        me.style.transform = "perspective(0) scale(1) translate3d(0,0,0) rotate3d(0,0,0,0)"
+    }else if (evn == 3){
+        works.style.transform = "perspective(0) scale(1) translate3d(0,0,0) rotate3d(0,0,0,0)"
+    }else if (evn == 4){
+        connect.style.transform = "perspective(0) scale(1) translate3d(0,0,0) rotate3d(0,0,0,0)"
+    }
+}
 
+// navbar
 document.addEventListener('scroll', function() {
     currentScrollValue = document.documentElement.scrollTop;
     nav.style.top = currentScrollValue +'px' ;
@@ -40,6 +54,8 @@ function btntest(btn){
         connect.style.display = "none";
         menu.style.display = "none";
         nav.style.display = "flex";
+        me.style.transform = "perspective(1000px) scale(0) translate3d(0,42vh,0) rotate3d(1,0,0,72deg)";
+        setTimeout(function(){ Evn(2); },300);
     }
     else if(btn==3){
         home.style.display = "none";
@@ -48,18 +64,23 @@ function btntest(btn){
         connect.style.display = "none";
         menu.style.display = "none";
         nav.style.display = "flex";
+        works.style.transform = "perspective(1000px) scale(0) translate3d(0,42vh,0) rotate3d(1,0,0,72deg)";
+        setTimeout(function(){ Evn(3); },300);
     }
     else if(btn==4){
         home.style.display = "none";
         me.style.display = "none";
         works.style.display = "none";
         connect.style.display = "flex";
+        connect.style.transform = "perspective(1000px) scale(0) translate3d(0,42vh,0) rotate3d(1,0,0,72deg)";
+        setTimeout(function(){ Evn(4); },300);
         menu.style.display = "none";
         nav.style.display = "flex";
     }else if(btn == 5){
-        home.style.transform = "perspective(0) scale(1) translate3d(0,0,0) rotate3d(0,0,0,0)";
         home.style.opacity = "1";
         home.style.display = "flex";
+        home.style.transform = "perspective(1000px) scale(0) translate3d(0,42vh,0) rotate3d(1,0,0,72deg)";
+        setTimeout(function(){ Evn(1); },300);
         nav.style.display = "none";
         me.style.display = "none";
         works.style.display = "none";
